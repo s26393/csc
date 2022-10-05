1c65e92a89de24c3b75b57c439a137c6d32690f5
s26393 <114556484+s26393@users.noreply.github.com>
2022-10-05 18:11:53 +0530

    Update script.js

--- bootstrap-5.2.2-dist/js/script.js
+++ bootstrap-5.2.2-dist/js/script.js
@@ -1,7 +1,7 @@
 $(function () {
   var includes = $('[data-include]')
   $.each(includes, function () {
-    var file = '/webpages/' + $(this).data('include') + '.html'
+    var file = '/csc/webpages/' + $(this).data('include') + '.html'
     $(this).load(file)
   })
 })
